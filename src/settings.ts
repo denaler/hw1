50e0bf3cbf0000ad68e123d0444d3ef9c2df4c33
Daler <kuperr9@gmail.com>
2023-10-20 18:58:02 +0700

    init commit

--- src/settings.ts
+++ src/settings.ts
@@ -188,7 +188,7 @@ app.put('/videos/:id', (req: RequestWithParamsAndBody<
         errors.errorsMessages.push({message: 'Invalid canBeDownloaded', field: 'canBeDownloaded'})
     }
 
-    if (minAgeRestriction !> 0 || minAgeRestriction !< 19) {
+    if (minAgeRestriction !> 0 && minAgeRestriction !< 19) {
         errors.errorsMessages.push({message: 'Invalid minAgeRestriction', field: 'minAgeRestriction'})
     }
 
